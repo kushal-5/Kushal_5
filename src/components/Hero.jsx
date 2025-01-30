d6ad6a87646bdc9636fbefa9b535a2a3e28bc383
Kushal Shrestha <shresthakushal850@gmail.com>
2025-01-30 12:16:14 +0545

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,17 +18,18 @@ const container = (delay) => ({
 const Hero = () => {
   return (
     <div className="border-b border-neutral-900 pb-4 lg:mb-36">
-      <div className="flex flex-wrap">
-        <div className="w-full lg:w-1/2">
-          <div className="flex flex-col items-center lg:items-start">
-            <motion.h1
-              variants={container(0)}
-              initial="hidden"
-              animate="visible"
-              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
-            >
-              Kushal Shrestha
-            </motion.h1>
+  <div className="flex flex-wrap">
+    <div className="w-full lg:w-1/2">
+      <div className="flex flex-col items-center justify-center lg:items-start lg:justify-start">
+        <motion.h1
+          variants={container(0)}
+          initial="hidden"
+          animate="visible"
+          className="pb-16 text-6xl font-thin tracking-tight text-center lg:mt-16 lg:text-8xl lg:text-left"
+        >
+          Kushal Shrestha
+        </motion.h1>
+
             <motion.span
               variants={container(0.5)}
               initial="hidden"
