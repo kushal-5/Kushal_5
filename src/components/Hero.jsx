7a61db19b79dd11c31120857a081f41e33d48745
Kushal Shrestha <shresthakushal850@gmail.com>
2025-01-30 12:37:45 +0545

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,6 @@
 import { HERO_CONTENT } from "../constants";
 import profilePic from "../assets/shadow.jpg";
 import { motion } from "framer-motion";
-import { GiHidden } from "react-icons/gi";
 
 const container = (delay) => ({
   hidden: { x: -100, opacity: 0 },
@@ -15,23 +14,40 @@ const container = (delay) => ({
   },
 });
 
+const rightContainer = (delay) => ({
+  hidden: { x: 100, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      delay: delay,
+    },
+  },
+});
+
 const Hero = () => {
   return (
     <div className="border-b border-neutral-900 pb-4 lg:mb-36">
-  <div className="flex flex-wrap">
-    <div className="w-full lg:w-1/2">
-      <div className="flex flex-col items-center justify-center lg:items-start lg:justify-start">
-        <motion.h1
+      <div className="flex flex-wrap">
+        <motion.div
           variants={container(0)}
           initial="hidden"
           animate="visible"
-          className="pb-16 text-6xl font-thin tracking-tight text-center lg:mt-16 lg:text-8xl lg:text-left"
+          className="w-full lg:w-1/2"
         >
-          Kushal Shrestha
-        </motion.h1>
+          <div className="flex flex-col items-center justify-center lg:items-start lg:justify-start">
+            <motion.h1
+              variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
+              className="pb-16 text-6xl font-thin tracking-tight text-center lg:mt-16 lg:text-8xl lg:text-left"
+            >
+              Kushal Shrestha
+            </motion.h1>
 
             <motion.span
-              variants={container(0.5)}
+              variants={container(0.75)}
               initial="hidden"
               animate="visible"
               className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"
@@ -47,8 +63,14 @@ const Hero = () => {
               {HERO_CONTENT}
             </motion.p>
           </div>
-        </div>
-        <div className="w-full lg:w-1/2 lg:p-8">
+        </motion.div>
+
+        <motion.div
+          variants={rightContainer(0.5)}
+          initial="hidden"
+          animate="visible"
+          className="w-full lg:w-1/2 lg:p-8"
+        >
           <div className="flex justify-center">
             <motion.img
               initial={{ x: 100, opacity: 0 }}
@@ -59,7 +81,7 @@ const Hero = () => {
               className="object-cover h-[500px] w-[500px] rounded-2xl"
             />
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
